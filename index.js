496b71c04f4bfe2af88fd135888e6f194a3bc4b1
GP <gregpetropoulos@yahoo.com>
2021-03-30 05:17:09 -0400

    I passed all four test by commenting out my conditionals for now, index.js set up underway

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const pageTemplate = require("./src/page-template.js");
+const generateTeam = require("./src/page-template.js");
+const team = [];
+const Manager = require("./lib/Manager");
+
 const questions = [
+  {
+    type: "list",
+    message: "What is your role?",
+    choices: ['Intern','Engineer', 'Manager', 'Employee']
+  },
   {
     type: "input",
     message: "What is your name?",
@@ -9,35 +17,36 @@ const questions = [
   },
   {
     type: "input",
-    message: "What is your ID number",
+    message: "What is your ID number?",
     name: "id",
   },
   {
     type: "input",
-    message: "What is your email",
+    message: "What is your email?",
     name: "email",
   },
+  {
+    type: "input",
+    message: "What university did you attend?",
+    name: "school",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub user name?",
+    name: "github",
+  }
 ];
 
-// TODO: Create a function to write to html, will place this in function init
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) =>
-    err ? console.log(err) : console.log("Success!")
-  );
-}
-
-// TODO: Create a function to initialize app
-function init() {
-  // prompt method calls in array of questions
-  inquirer
-    .prompt(questions)
-    // promise, answers function takes answer responses and writes to readme
-    .then((answers) => {
-      console.log(answers);
-      const response = pageTemplate(answers);
-      writeToFile("index.html", response);
+function manager() {
+  inquirer.prompt(questions)
+    .then((data) => {
+      cosnt newManager = new Manager(data.name, data.id, data.email, data.officeNumber)
+      team.push(newManager);
     });
-}
-
-// Function call to initialize app
-init();
+    
+  }
+  
+  
+  //     fs.writeFile("index.html", generateTeam(data), (err) =>
+  //        err ? console.log(err) : console.log('Success!')
+  //  );
