51f9734fa62115f994e838490410dc7c3dba7371
GP <gregpetropoulos@yahoo.com>
2021-03-30 20:52:42 -0400

    Working out the fs and initiate functions

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+// node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateTeam = require("./src/page-template.js");
-const team = [];
+
+// lib modules
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+const newStaffMemberData = [];
+
 
 const questions = [
   {
     type: "list",
     message: "What is your role?",
-    choices: ['Intern','Engineer', 'Manager', 'Employee']
+    choices: ["Employee", "Engineer", "Intern", "Manager"],
   },
   {
     type: "input",
@@ -34,19 +42,71 @@ const questions = [
     type: "input",
     message: "What is your GitHub user name?",
     name: "github",
-  }
+  },
 ];
 
-function manager() {
-  inquirer.prompt(questions)
-    .then((data) => {
-      cosnt newManager = new Manager(data.name, data.id, data.email, data.officeNumber)
-      team.push(newManager);
+// function team wraps all the employee, engineer, intern, manager function and their user inputs  pushed into an array
+function teamData() {
+  // takes user inputs from questions array and with the promise method, will push into teamArr
+  // HOW TO DRY it out?
+  function employee() {
+    inquirer.prompt(questions).then((data) => {
+      const newEmployee = new Employee(data.name, data.id, data.email);
+      newStaffMemberData.push(newEmployee);
     });
-    
   }
-  
-  
-  //     fs.writeFile("index.html", generateTeam(data), (err) =>
-  //        err ? console.log(err) : console.log('Success!')
-  //  );
+
+  function engineer() {
+    inquirer.prompt(questions).then((data) => {
+      const newEngineer = new Engineer(
+        data.name,
+        data.id,
+        data.email,
+        data.github
+      );
+      newStaffMemberData.push(newEngineer);
+    });
+  }
+
+  function intern() {
+    inquirer.prompt(questions).then((data) => {
+      const newIntern = new Intern(
+        data.name,
+        datae.id,
+        data.email,
+        data.school
+      );
+      newStaffMemberData.push(newIntern);
+    });
+  }
+
+  function manager() {
+    inquirer.prompt(questions).then((data) => {
+      const newManager = new Manager(
+        data.name,
+        data.id,
+        data.email,
+        data.officeNumber
+      );
+      newStaffMemberData.push(newManager);
+    });
+  }
+}
+
+// Write the page-template file to the html file that is created in this function
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Success!")
+  );
+}
+
+// Create function to intitilaize app
+// STUCK HERE---------
+function init() {
+  const res = teamData(newStaffMemberData)
+  const response = generateTeam(team);
+    writeToFile("index.html", response);
+    writeToFile('index.html',res)
+  }
+
+init()
