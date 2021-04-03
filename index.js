615a938967a856cce44d393a374e8fd7d7b07730
GP <gregpetropoulos@yahoo.com>
2021-04-03 15:33:31 -0400

    fully functional after heavy restructring of the await to replace the .then promises

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const generateTeam = require("./src/page-template.js");
 
 // lib modules
-const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
@@ -13,8 +12,8 @@ const Manager = require("./lib/Manager");
 const newStaffMemberData = [];
 
 // Array object questions asked in CLI to user
-const questions = () => {
-  return inquirer
+const questions = async () => {
+  const answers = await inquirer
     .prompt([
       {
         type: "input",
@@ -35,14 +34,16 @@ const questions = () => {
         type: "list",
         message: "What is your role?",
         name: "role",
-        choices: ["Employee", "Engineer", "Intern", "Manager"],
+        choices: ["Engineer", "Intern", "Manager"],
       },
     ])
-    .then((answers) => {
-      console.log(answers);
+
+
+    
+    //  console.log(answers);
       // if manager selected, answer these specific question
       if (answers.role === "Manager") {
-        inquirer
+        const managerAns = await inquirer
           .prompt([
             {
               type: "input",
@@ -50,39 +51,35 @@ const questions = () => {
               name: "officeNumber",
             },
           ])
-          .then((managerAns) => {
-            const newManager = new Manager(
-              answers.name,
-              answers.id,
-              answers.email,
-              managerAns.officeNumber
-            );
-            console.log("managerAns");
-          });
-
+          const newManager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            managerAns.officeNumber
+          );
+          newStaffMemberData.push(newManager);
+          
         // if engineer selected answer these set of questions
       } else if (answers.role === "Engineer") {
-        inquirer
+        const githubAns = await inquirer
           .prompt([
             {
               type: "input",
               message: "What is your GitHub user name?",
               name: "github",
-            },
+            }
           ])
-          .then((githubAns) => {
             const newEngineer = new Engineer(
               answers.name,
               answers.id,
               answers.email,
               githubAns.github
             );
-            console.log("engineer");
-          });
+            newStaffMemberData.push(newEngineer);
           
         // if intern selected answer these set of questions
       } else if (answers.role === "Intern") {
-        inquirer
+        const internAns = await inquirer
           .prompt([
             {
               type: "input",
@@ -90,32 +87,46 @@ const questions = () => {
               name: "school",
             },
           ])
-          .then((internAns) => {
-            const newIntern = new Intern(
-              answers.name,
-              answers.id,
-              answers.email,
-              internAns.school
-            );
-            console.log("intern");
-          });
-      } else {
-        //  const newEmployee = new Employee();
-        //  return newEmployee;
-      }
-    });
-    newStaffMemberData.push(questions)
-};
-questions();
+          
+          const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            internAns.school
+          );
+          newStaffMemberData.push(newIntern);          
+      } 
 
-// Write the page-template file to the html file that is created in this function
-// Create the output directory if the output path doesn't exist
-if (!fs.existsSync("./output")) {
-  fs.mkdirSync("./output");
+}; //end of questions function
+
+async function promptQuestions() {
+  await questions()
+    
+  
+  const addMemberAns = await inquirer
+    .prompt([
+      {
+        name:'addMember',
+        type: 'list',
+        choices: ['Add a new member', 'Create team'],
+        message: "What would you like to do next?"
+      }
+    ])
+
+    if (addMemberAns.addMember === 'Add a new member') {
+      return promptQuestions()
+    }
+    return createTeam();
+}  
+
+promptQuestions();
+
+function createTeam () {
+  console.log("new guy", newStaffMemberData)
+  fs.writeFileSync(
+    "./output/index.html",
+    generateTeam(newStaffMemberData),
+    "utf-8"
+  );
 }
 
-fs.writeFileSync(
-  "./output/index.html",
-  generateTeam(newStaffMemberData),
-  "utf-8"
-);
