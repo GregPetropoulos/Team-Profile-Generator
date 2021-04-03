1cc7d009db6533e785631c7479b2b033b431012b
GP <gregpetropoulos@yahoo.com>
2021-04-03 07:34:22 -0400

    Refactored the questions function and was able to establish CLI prompts with the manager, just need to finsih the intern and engineer questions

--- index.js
+++ index.js
@@ -13,126 +13,116 @@ const Manager = require("./lib/Manager");
 const newStaffMemberData = [];
 
 // Array object questions asked in CLI to user
-const questions = [
-  {
-    type: "list",
-    message: "What is your role?",
-    choices: ["Employee", "Engineer", "Intern", "Manager"],
-  },
-  {
-    type: "input",
-    message: "What is your name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is your ID number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is your email?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What university did you attend?",
-    name: "school",
-  },
-  {
-    type: "input",
-    message: "What is your GitHub user name?",
-    name: "github",
-  },
-];
+const questions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "What is your role?",
+        name: "role",
+        choices: ["Employee", "Engineer", "Intern", "Manager"],
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      // if manager selected answer this specific question
+      if (answers.role === "Manager") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your office number",
+              name: "officeNumber",
+            },
+          ])
+          .then((managerAns) => {
+            const newManager = new Manager(
+              answers.name,
+              answers.id,
+              answers.email,
+              managerAns.officeNumber
+            );
+            console.log(managerAns);
+          });
 
-// function teamData wraps all the employee, engineer, intern, manager functions
-// Take user answers pushed into newStaffMemberData array
-// fs write the generateTeam to output directory
-function teamData() {
-  // takes user inputs from questions array and with the promise method, will push into newStaffMemberData
-  // HOW TO DRY it out?
-  function employee() {
-    inquirer.prompt(questions).then((data) => {
-      const newEmployee = new Employee(data.name, data.id, data.email);
-      newStaffMemberData.push(newEmployee);
+        // if engineer selected answer these set of questions
+      } else if (answers.role === "Engineer") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your GitHub user name?",
+              name: "github",
+            },
+          ])
+          .then((githubAns) => {
+            const newEngineer = new Engineer(
+              answers.name,
+              answers.id,
+              answers.email,
+              githubAns.github
+            );
+            console.log("engineer");
+          });
+        // if intern selected answer these set of questions
+      } else if (answers.role === "Intern") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What university did you attend?",
+              name: "school",
+            },
+          ])
+          .then((internAns) => {
+            const newIntern = new Intern(
+              answers.name,
+              answers.id,
+              answers.email,
+              internAns.school
+            );
+            console.log("intern");
+          });
+          // if employee selected ask the base questions
+      } else (answers.role === "Employee")
+       questions().then((employeeAns) => {
+         const newEmployee = new Employee(
+           answers.name, 
+           answers.id, 
+           answers.email,
+            employeeAns
+            );
+        console.log("employee");
+    
     });
-  }
+});
+};
 
-  function engineer() {
-    inquirer.prompt(questions).then((data) => {
-      const newEngineer = new Engineer(
-        data.name,
-        data.id,
-        data.email,
-        data.github
-      );
-      newStaffMemberData.push(newEngineer);
-    });
-  }
 
-  function intern() {
-    inquirer.prompt(questions).then((data) => {
-      const newIntern = new Intern(
-        data.name,
-        datae.id,
-        data.email,
-        data.school
-      );
-      newStaffMemberData.push(newIntern);
-    });
-  }
 
-  function manager() {
-    inquirer.prompt(questions).then((data) => {
-      const newManager = new Manager(
-        data.name,
-        data.id,
-        data.email,
-        data.officeNumber
-      );
-      newStaffMemberData.push(newManager);
-    });
-  }
-  // -------------stuck
-
-  // Write the page-template file to the html file that is created in this function
-  // Create the output directory if the output path doesn't exist
-  if (!fs.existsSync("./output")) {
-    fs.mkdirSync("./output");
-  }
-  fs.writeFileSync(
-    "./output/index.html",
-    generateTeam(newStaffMemberData),
-    "utf-8"
-  );
-}
-// end of teamData block
-// teamData();
-
-// Function to add staff members with specific Q&A from teamData
-// Dynamicaly generate the html card
-function addTeamMembers() {
-  // if manager is selected in the first question, execute manager() and generateManager()
-  if (questions.choices === "Manager") {
-    manager();
-    generateManager();
-  } else if (questions.choices === "Engineer") {
-    engineer();
-    generateEngineer();
-  } else if (questions.choices === "Intern") {
-    intern();
-    generateIntern();
-  } else if (questions.choices === "Employee") {
-    employee();
-    generateEmployee()
-  }
-  return questions;
+// Write the page-template file to the html file that is created in this function
+// Create the output directory if the output path doesn't exist
+if (!fs.existsSync("./output")) {
+  fs.mkdirSync("./output");
 }
 
-// Start app
-function startApp(){
-  teamData()
-  addTeamMembers()
-}
-startApp()
+fs.writeFileSync(
+  "./output/index.html",
+  generateTeam(newStaffMemberData),
+  "utf-8"
+);
